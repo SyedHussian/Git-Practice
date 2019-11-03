4db2cbb57bc997a965514be95b11f7c62b8248dd
Hamidur Rahman <Hamidur0405@gmail.com>
2019-11-02 22:15:40 -0400

    initiating git practice project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        testHamid();
+    }
+    
+    public static void testHamid()
+    {
+        System.out.println("hello world!");
+    }
+}
